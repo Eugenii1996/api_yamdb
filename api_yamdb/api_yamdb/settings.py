82957d1732560f9ddf15315a5b5096d8fb7e1d82
Evgenii <jonmir.2@gmail.com>
2022-03-29 19:14:59 +0300

    Созданы приложения api и reviews

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,6 +21,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    'reviews',
+    'api',
 ]
 
 MIDDLEWARE = [
