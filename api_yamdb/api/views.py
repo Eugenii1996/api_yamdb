357ff65248b52b99601837c70bb6a35d5a0b6d31
Владимир Ветровd:/Dev/backend_test_homework/venv/Scripts/python.exe <vsvetrov@gmail.com>
2022-03-30 06:16:14 +0300

    draft

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from reviews.models import Comment, Review, Title
+from .permissions import IsOwnerOrReadOnly
+
+from .serializers import (CommentSerializer, ReviewSerializer)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+    pagination_class = LimitOffsetPagination
+
+    def get_queryset(self):
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id')) 
+        # проверить title_id или post_id
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+#надо понять в чем отличие от комментария
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+    pagination_class = LimitOffsetPagination
+
+    def get_queryset(self):
+        review = get_object_or_404(Post, pk=self.kwargs.get('title_id'))
+        # проверить title_id или post_id или review_id
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+        #требуется доработка
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Review(models.Model):
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name='reviews', 
+        verbose_name='Автор'
+    )
+    title = models.ForeignKey(
+        Title, 
+        on_delete=models.CASCADE, 
+        related_name='reviews', 
+        verbose_name='Произведение'
+    )
+    text = models.TextField()
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True)
+    
+
+
+
