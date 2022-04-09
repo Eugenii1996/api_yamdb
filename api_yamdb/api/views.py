7aff18f28f466418fe3c78981606e591d35e865f
Владимир Ветровd:/Dev/backend_test_homework/venv/Scripts/python.exe <vsvetrov@gmail.com>
2022-04-09 07:08:42 +0300

    testing

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -24,8 +24,9 @@ class ReviewViewSet(viewsets.ModelViewSet):
         return title.reviews.all()
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-#надо понять в чем отличие от класса комментариев
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        serializer.save(author=self.request.user, title=title)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
@@ -39,8 +40,10 @@ class CommentViewSet(viewsets.ModelViewSet):
         return review.comments.all()
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-        #требуется доработка
+        title_id = self.kwargs.get('title_id')
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, id=review_id, title=title_id)
+        serializer.save(author=self.request.user, review=review)
 
 
 class CategoryViewSet(CreateGetDeleteViewSet):
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,7 +1,6 @@
-from django.contrib.auth import get_user_model
 from django.db import models
+from users.models import User
 
-User = get_user_model()
 
 class Category(models.Model):
     name = models.CharField(max_length=256)
