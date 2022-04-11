7f564a8fe7563aad6d87d2276df92f044726435e
Владимир Ветровd:/Dev/backend_test_homework/venv/Scripts/python.exe <vsvetrov@gmail.com>
2022-04-11 06:07:57 +0300

    one change

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -86,10 +86,10 @@ class ReviewSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
         request = self.context['request']
-        author = request.user
-        title_id = self.context['view'].kwargs.get('title_id')
-        title = get_object_or_404(Title, pk=title_id)
         if request.method == 'POST':
+            author = request.user
+            title_id = self.context['view'].kwargs.get('title_id')
+            title = get_object_or_404(Title, pk=title_id)
             if Review.objects.filter(title=title, author=author).exists():
                 raise ValidationError('Вы не можете добавить более'
                                       'одного отзыва на произведение')
